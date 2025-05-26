e0cb53f109c96cfc3c377f78c969a72ee4bf5721
Danburen <2964361672@qq.com>
2025-05-26 09:28:46 +0800

    upload projects

---
+++ nuxt.config.ts
@@ -0,0 +1,37 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import {fileURLToPath} from "node:url";
+
+
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  srcDir: 'src/', // resource
+  alias:{
+    '@': fileURLToPath(new URL('./src', import.meta.url)),
+    '~': fileURLToPath(new URL('./src', import.meta.url))
+  },
+  modules:['@nuxtjs/i18n',
+    ['@element-plus/nuxt',{importStyle: 'css', themes: ['dark']}],
+    '@pinia/nuxt'],
+  plugins: ['@/plugins/async-validator.client'],
+  i18n:{
+    strategy: 'no_prefix', // no prefix router
+    detectBrowserLanguage: {
+        useCookie: true,     // use cookie to store language
+        cookieKey: 'i18n_redirected'
+    },
+    locales: [
+        { code: 'en' , file: "en_US.json"},
+        { code: 'zh' , file: "zh_CN.json"},
+    ],
+    lazy: true,
+    langDir: 'locales',
+  },
+
+
+  // typescript: {
+  //   tsConfig: {
+  //     extends: './tsconfig.json'
+  //   }
+  // }
+})
---
+++ src/app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import test from '@/test.vue'
+// const { apiBase } = useRuntimeConfig().public;
+// console.log(apiBase);
+// console.log(process.env.VITE_API_BASE);
+// console.log(import.meta.env.VITE_API_BASE);
+</script>
+<template>
+  <div>
+    <NuxtPage></NuxtPage>
+  </div>
+</template>
