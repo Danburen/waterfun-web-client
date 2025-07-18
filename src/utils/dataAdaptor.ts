25bce2889da41063147dc647021956d6a8bbe790
Danburen <2964361672@qq.com>
2025-07-18 22:26:53 +0800

    feat: add base64 transformer

--- src/utils/dataAdaptor.ts
+++ src/utils/dataAdaptor.ts
@@ -8,3 +8,19 @@ export function convertArrayBufferToBase64(buffer: ArrayBuffer) {
         (data, byte) => data + String.fromCharCode(byte), ''
     ));
 }
+
+export function base64ToUint8Array(base64: string) {
+    const binaryString = atob(base64);
+    const bytes = new Uint8Array(binaryString.length);
+    for (let i = 0; i < binaryString.length; i++) {
+        bytes[i] = binaryString.charCodeAt(i);
+    }
+    return bytes;
+}
+
+export async function base64ToText(base64: string) {
+    const blob = new Blob([Uint8Array.from(atob(base64), c => c.charCodeAt(0))], {
+        type: 'text/plain;charset=utf-8'
+    });
+    return await new Response(blob).text();
+}
